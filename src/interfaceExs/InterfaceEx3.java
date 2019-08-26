ed8498338bbb1779a6196be51ec476ba71654122
dewytear <youngjinkwak@gmail.com>
2019-08-26 15:07:47 +0900

    20190826 PolymorphismEx1

--- src/interfaceExs/InterfaceEx3.java
+++ src/interfaceExs/InterfaceEx3.java
@@ -12,6 +12,7 @@ abstract class AbsClass{
 	abstract void c();
 }
 
+// InterfaceF 는 InterfaceA 와 InterfaceB 에 접근 가능
 interface InterfaceF extends InterfaceA, InterfaceB{
 	void d();
 }
@@ -36,6 +37,7 @@ public class InterfaceEx3 extends AbsClass implements InterfaceF {
 	
 	public static void main(String[] args) {
 
+		// 다형성: 하나의 인터페이스로 여러개의 객체에 접근 할 수 있다.
 		InterfaceEx3 if3 = new InterfaceEx3();
 		if3.a();
 		System.out.println(if3.b());
@@ -43,13 +45,15 @@ public class InterfaceEx3 extends AbsClass implements InterfaceF {
 		if3.d();
 		System.out.println("------");
 		
+		// 부모클래스(AbsClass)로 자식인터페이스 생성
 		AbsClass ac = new InterfaceEx3();
 //		ac.a();
 //		System.out.println(intF.b());
 		ac.c();
 //		ac.d();
 		System.out.println("------");
-		
+
+		// 부모인터페이스(InterfaceF)로 자식인터페이스 생성
 		InterfaceF intF = new InterfaceEx3();
 		intF.a();
 		System.out.println(intF.b());
